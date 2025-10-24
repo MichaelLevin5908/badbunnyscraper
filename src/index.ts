81471b19200dc7e5d924a3e9e2a4401f4ec0986d
michaelLevin5908 <michaeljustinlevin@gmail.com>
2025-10-24 16:17:23 -0700

    remove useless

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ dotenv.config();
 async function main() {
   const agent = new BskyAgent({ service: 'https://bsky.social' });
 
-  // Login with your credentials from .env
   await agent.login({
     identifier: process.env.IDENTIFIER!,
     password: process.env.PASSWORD!
   });
 
-  // Search for posts with #badbunny or #superbowl
   const response = await agent.api.app.bsky.feed.searchPosts({
     q: '#badbunny OR #superbowl',
     limit: 100
